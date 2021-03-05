4667b68aa9acb090e7656ca56fd86175fc3ccad5
jamie simpson <jamiesimpson08@gmail.com>
2021-03-05 11:13:07 -0700

    final push

---
+++ react-counter-app-main/src/component/NavBar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+function NavBar({totalCounters}, {test}){
+    
+        return (
+        <nav className="navbar navbar-light bg-light">
+            <div className="navbar-brand">
+               <i className="fa fa-shopping-cart fa-lg m-2" aria-hidden="true" />
+               <span className="badge badge-pill badge-info m-2" style={{ width: 50 }}>
+                  {totalCounters}
+            </span>
+            Items
+            </div>
+        </nav>
+        )
+            
+    }
+
+
+export default NavBar
